7c786eb031b94cd8ca2c8a6c3cb3a1d5627a8a77
devkarim <devkarim@hotmail.com>
2022-08-09 22:00:29 +0200

    make navbar responsive

--- src/components/guest/Appbar.tsx
+++ src/components/guest/Appbar.tsx
@@ -1,19 +1,25 @@
 import classNames from 'classnames';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import Button from '../ui/Button';
 import GradientText from '../ui/GradientText';
+import IconLink from '../ui/IconLink';
+import { FaBars } from 'react-icons/fa';
 
 interface AppbarProps {
   children?: React.ReactNode;
-  className?: string;
 }
 
-const Appbar: React.FC<AppbarProps> = ({ className }) => {
+interface MobileNavbarProps {
+  isNavbarExpanded: boolean;
+  toggle: () => any;
+}
+
+const DesktopAppbar: React.FC<{}> = () => {
   return (
-    <div
+    <nav
       className={classNames(
-        'appbar z-50 flex justify-between items-center py-6 px-10 lg:px-20',
-        className
+        'appbar z-50 justify-between items-center py-6 lg:pl-28 px-20 hidden md:flex'
       )}
     >
       <GradientText className="text-4xl font-pacifico bg-gradient-to-r from-primary to-secondary">
@@ -27,6 +33,60 @@ const Appbar: React.FC<AppbarProps> = ({ className }) => {
           Contact
         </Button>
       </div>
+    </nav>
+  );
+};
+
+const MobileAppbar: React.FC<MobileNavbarProps> = ({
+  isNavbarExpanded,
+  toggle,
+}) => {
+  return (
+    <div>
+      <nav className="appbar z-50 justify-between items-center py-6 px-10 flex md:hidden">
+        <GradientText className="text-4xl font-pacifico bg-gradient-to-r from-primary to-secondary">
+          devkarim
+        </GradientText>
+        <IconLink className="text-2xl" onClick={toggle}>
+          <FaBars />
+        </IconLink>
+      </nav>
+
+      {isNavbarExpanded && (
+        <div className="flex flex-col text-xl font-medium mt-4 py-6 px-6 absolute w-full h-screen space-y-8 bg-dark dark:bg-appbar-dark md:hidden">
+          <Link href="/services">Services</Link>
+          <Link href="/work">Work</Link>
+          <Link href="/reviews">Reviews</Link>
+          <Button className="w-[100%] bg-gradient-to-r from-primary to-secondary">
+            Contact
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Appbar: React.FC<AppbarProps> = ({}) => {
+  const [isNavbarExpanded, setNavbarExpanded] = useState(false);
+
+  useEffect(() => {
+    if (isNavbarExpanded) {
+      document.body.classList.add('!overflow-y-hidden', 'md:!overflow-y-auto');
+    } else {
+      document.body.classList.remove(
+        '!overflow-y-hidden',
+        'md:!overflow-y-auto'
+      );
+    }
+  }, [isNavbarExpanded]);
+
+  return (
+    <div>
+      <DesktopAppbar />
+      <MobileAppbar
+        isNavbarExpanded={isNavbarExpanded}
+        toggle={() => setNavbarExpanded(!isNavbarExpanded)}
+      />
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,49 @@ import Appbar from '../components/guest/Appbar';
 import SpaceCraft from '../components/guest/Spacecraft';
 import Button from '../components/ui/Button';
 import { __dev__ } from '../config/constants';
-// import GradientText from '../components/ui/GradientText';
 
 const TEXT_PAUSE = __dev__ ? 1000 : 3000;
+
+const LeftBlobs = () => (
+  <Image src="/svg/left-blobs.svg" width={120} height={120} />
+);
+
+const ServiceTypeWriter = () => (
+  <Typewriter
+    options={{
+      loop: true,
+      cursorClassName: 'ml-2 text-6xl',
+      delay: 150,
+      wrapperClassName:
+        'text-center w-auto pl-2 text-transparent bg-clip-text text-7xl lg:text-8xl xl:text-10xl font-oleo bg-gradient-to-r from-primary to-secondary',
+    }}
+    onInit={(typewriter) => {
+      typewriter
+        .typeString('Web')
+        .pauseFor(TEXT_PAUSE)
+        .deleteAll()
+        .typeString('Desktop')
+        .pauseFor(TEXT_PAUSE)
+        .deleteAll()
+        .typeString('Mobile')
+        .pauseFor(TEXT_PAUSE)
+        .deleteAll()
+        .start();
+    }}
+  />
+);
+
+const RightWaves = () => (
+  <Image src="/svg/right-waves.svg" layout="fill" objectFit="cover" />
+);
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col relative h-screen">
       <div className="absolute hidden lg:block">
-        <Image src="/svg/left-blobs.svg" width={120} height={120} />
+        <LeftBlobs />
       </div>
-      <Appbar className="lg:pl-28" />
+      <Appbar />
       <div className="grid justify-center lg:grid-cols-2 lg:grid-flow-col py-6 lg:px-8 xl:px-16 items-center h-full">
         <div className="flex flex-col items-center lg:block">
           <div className="flex text-center lg:text-start">
@@ -25,31 +57,10 @@ const Home: NextPage = () => {
               <h1 className="text-4xl md:text-5xl font-bold mb-4">
                 I make apps on
               </h1>
-              <Typewriter
-                options={{
-                  loop: true,
-                  cursorClassName: 'ml-2 text-6xl',
-                  delay: 150,
-                  wrapperClassName:
-                    'text-center w-auto pl-2 text-transparent bg-clip-text text-7xl lg:text-8xl xl:text-10xl font-oleo bg-gradient-to-r from-primary to-secondary',
-                }}
-                onInit={(typewriter) => {
-                  typewriter
-                    .typeString('Web')
-                    .pauseFor(TEXT_PAUSE)
-                    .deleteAll()
-                    .typeString('Desktop')
-                    .pauseFor(TEXT_PAUSE)
-                    .deleteAll()
-                    .typeString('Mobile')
-                    .pauseFor(TEXT_PAUSE)
-                    .deleteAll()
-                    .start();
-                }}
-              />
+              <ServiceTypeWriter />
             </div>
           </div>
-          <Button className="lg:ml-8 mt-12 w-52 md:w-64 lg:w-36 h-10 text-xl">
+          <Button className="lg:ml-8 mt-12 w-64 lg:w-36 h-10 text-xl">
             Let's Talk
           </Button>
         </div>
@@ -58,7 +69,7 @@ const Home: NextPage = () => {
         </div>
       </div>
       <div className="absolute top-0 right-0 hidden xl:block w-20 h-full">
-        <Image src="/svg/right-waves.svg" layout="fill" objectFit="cover" />
+        <RightWaves />
       </div>
     </div>
   );
